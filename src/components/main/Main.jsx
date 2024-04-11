6f3da657ad72ba013a91ca199ba575f0ef442d1f
NVadim <palaginvad@gmail.com>
2024-04-11 20:42:26 +0300

    fix

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -51,7 +51,7 @@ const Main = ({ user }) => {
 
 	const currentTimeStamp = Math.floor(Date.now() / 1000);
 	const futureTimestamp = currentTimeStamp + 60 * 60; // 60 * 60
-	const remainingTime = value.active_at - currentTimeStamp;
+	const remainingTime = user.active_at - currentTimeStamp;
 	const placeholderTIme = futureTimestamp - currentTimeStamp;
 
 	const pauseGame = () => {
