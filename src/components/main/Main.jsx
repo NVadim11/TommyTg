ac5646082bae98993de253916f2153f9eb02538f
Ekkaira <denysxchebotar@gmail.com>
2024-05-31 13:18:07 +0300

    g-tag

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -99,7 +99,7 @@ const Main = ({ user }) => {
 		};
 		const dateStringWithTime = now.toLocaleString('en-GB', options);
 
-		fetch(testURL + '/api/set-activity', {
+		fetch(secretURL + '/api/set-activity', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -144,7 +144,7 @@ const Main = ({ user }) => {
 	const getGameStatus = async () => {
 		try {
 			const initGameStatusCheck = await axios.get(
-				testURL + `/api/telegram-id/${userId}`
+				secretURL + `/api/telegram-id/${userId}`
 			);
 		} catch (e) {
 			console.log('Error fetching leaderboard data');
